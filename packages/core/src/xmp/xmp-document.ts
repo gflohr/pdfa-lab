01deae5d520d688523b1f2d9bf37de48946fa555
Guido Flohr <guido.flohr@cantanea.com>
2026-09-16 20:28:13 +0300

    remove dead code

--- packages/core/src/xmp/xmp-document.ts
+++ packages/core/src/xmp/xmp-document.ts
@@ -233,9 +233,7 @@ export class XmpDocument {
 
 	/** @internal */
 	public serialiseXmp(): string {
-		// FIXME! The clean-up is unnecessary!
 		const output = this.serialise('application/rdf+xml', undefined)
-			.replace(/^( {4})+/gm, (match) => '\t'.repeat(match.length / 4))
 			.replace(/\n$/, '')
 			.replace(/^/gm, '\t');
 		return `<?xpacket begin="${bom}" id="W5M0MpCehiHzreSzNTczkc9d"?>
